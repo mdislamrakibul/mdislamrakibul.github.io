882b3d59bd10814f44a579c6bcc69f9905345fbc
rakib-developer <rakibul.nexkraft@gmail.com>
2023-10-07 13:05:46 -0400

    Third Commit

---
+++ assets/js/education.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+  loadFull(tsParticles);
+
+  $('#education')
+    .particles()
+    .init(
+      {
+        background: {
+          color: {
+            value: 'transparent',
+          },
+        },
+        fpsLimit: 50,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            onHover: {
+              enable: true,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#ffffff',
+          },
+          links: {
+            color: '#ffffff',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 0.5,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outModes: {
+              default: 'bounce',
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            value: { min: 1, max: 3 },
+          },
+        },
+        detectRetina: true,
+      },
+      function (container) {
+        // container is the particles container where you can play/pause or stop/start.
+        // the container is already started, you don't need to start it manually.
+      },
+    );
+});
